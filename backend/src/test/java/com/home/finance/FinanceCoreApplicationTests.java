48371bf6afa351b79cc8276058c7c7a7e7f6fa6c
Matas <hakasmatas@gmail.com>
2026-04-28 19:22:32 +0300

    Initial commit

---
+++ backend/src/test/java/com/home/finance/FinanceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
